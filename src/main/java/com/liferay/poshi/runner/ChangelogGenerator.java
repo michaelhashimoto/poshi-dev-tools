c2ef3aeab46b4067cde823eacebdb919771e486e
Kenji Heigel <kenji.heigel@liferay.com>
2020-06-16 10:57:20 -0700

    Update changelog generator

--- src/main/java/com/liferay/poshi/runner/ChangelogGenerator.java
+++ src/main/java/com/liferay/poshi/runner/ChangelogGenerator.java
@@ -14,20 +14,29 @@
 
 package com.liferay.poshi.runner;
 
+import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.domain.BasicComponent;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.input.LinkIssuesInput;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.revwalk.RevCommit;
+import com.liferay.poshi.runner.util.FileUtil;
+import com.liferay.poshi.runner.util.RegexUtil;
+import com.liferay.poshi.runner.util.StringUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import java.net.URI;
-
 import java.net.URLEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -37,24 +46,76 @@ import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+/**:
  * @author Kenji Heigel
  */
 public class ChangelogGenerator {
+
+	public static final String RELEASE_TICKET = "POSHI-87";
+	public static final String PORTAL_DIR =
+		"/Users/kenjiheigel/Projects/github/liferay-portal/master";
+
 	public static void main(String[] args) throws Exception {
-		File gitWorkDir = new File(
-			"/Users/kenji/Projects/github/com-liferay-poshi-runner");
+		File gitWorkDir = new File(PORTAL_DIR);
+
+		String poshiDirPath = "modules/test/poshi-runner";
+
+		String bndPath = poshiDirPath + "/poshi-runner/bnd.bnd";
+
+		String changelogPath = poshiDirPath + "/CHANGELOG.markdown";
+
+		File changelogFile = new File(gitWorkDir, changelogPath);
 
 		Git git = Git.open(gitWorkDir);
 
-		ObjectId lastPoshiReleaseSHA = ObjectId.fromString(
-			"37033d4d9c84a0fcbc66cc83704263756ca74c5b");
+		Repository repository = git.getRepository();
 
-		ObjectId newPoshiReleaseSHA = ObjectId.fromString(
-			"1745038d7a647711d0b81faaf218f09cff8a9590");
+		ObjectId newReleaseSHA = repository.resolve(repository.getBranch());
 
-		Iterable<RevCommit> commits =
-			git.log().addRange(lastPoshiReleaseSHA, newPoshiReleaseSHA).call();
+		Iterable<RevCommit> bndCommits = git.log(
+		).add(
+			newReleaseSHA
+		).addPath(
+			bndPath
+		).setMaxCount(
+			50
+		).call();
+
+		ObjectId lastReleaseSHA = null;
+
+		int i = 1;
+		String releaseVersion = "";
+
+		for (RevCommit commit : bndCommits) {
+			String content = _getFileContentAtCommit(git, commit, bndPath);
+
+			if (i == 2) {
+				releaseVersion = RegexUtil.getGroup(
+					content, "Bundle-Version:[\\s]*(.*)", 1);
+			}
+
+			if (content.contains(_getLastReleasedVersion(changelogFile))) {
+				lastReleaseSHA = commit.getId();
+
+				break;
+			}
+
+			i++;
+		}
+
+		Iterable<RevCommit> commits = git.log(
+		).addRange(
+			lastReleaseSHA, newReleaseSHA
+		).addPath(
+			poshiDirPath
+		).call();
 
 		Set<String> tickets = new TreeSet<>();
 
@@ -63,7 +124,8 @@ public class ChangelogGenerator {
 
 			commitMessage = commitMessage.trim();
 
-			Pattern ticketPattern = Pattern.compile("LRQA[\\S]*");
+			Pattern ticketPattern = Pattern.compile(
+				"(LPS|LRQA|LRCI|POSHI)[\\S]*");
 
 			Matcher matcher = ticketPattern.matcher(commitMessage);
 
@@ -82,82 +144,284 @@ public class ChangelogGenerator {
 		JiraRestClient jiraRestClient =
 			jiraRestClientFactory.createWithBasicHttpAuthentication(
 				uri, Authentication.JIRA_USERNAME,
-					Authentication.JIRA_PASSWORD);
+				Authentication.JIRA_PASSWORD);
 
 		Map<String, List<String>> ticketGroups = new TreeMap<>();
 
 		String ticketListString = tickets.toString();
 
-		ticketListString = ticketListString.replace("[", "(");
-		ticketListString = ticketListString.replace("]", ")");
+		ticketListString = StringUtil.replace(ticketListString, "[", "(");
+		ticketListString = StringUtil.replace(ticketListString, "]", ")");
 
 		ticketListString = URLEncoder.encode(ticketListString, "UTF-8");
 
-
 		System.out.println(
 			"https://issues.liferay.com/issues/?jql=key%20in" +
-			ticketListString);
+				ticketListString);
+
+		IssueRestClient issueRestClient = jiraRestClient.getIssueClient();
+
+		Issue releaseIssue = issueRestClient.getIssue(
+		RELEASE_TICKET
+		).claim();
+
+		if (releaseIssue.getStatus().getName().equals("Closed")) {
+			throw new RuntimeException(
+				"https://issues.liferay.com/browse/" + RELEASE_TICKET +
+					" is closed. Verify correct ticket.");
+		}
 
 		for (String ticketID : tickets) {
-			Issue issue =
-				jiraRestClient.getIssueClient().getIssue(ticketID).claim();
+			Issue issue = issueRestClient.getIssue(
+				ticketID
+			).claim();
 
-			boolean missingLabel = true;
+			String ticketMessage =
+				_getTicketMarkdownURL(ticketID) + " - " + issue.getSummary();
 
-			for (String label : issue.getLabels()) {
-				if (label.startsWith("poshi_")) {
-					if (!ticketGroups.containsKey(label)) {
+			ticketMessage = ticketMessage.trim();
+
+			LinkIssuesInput linkIssuesInput = new LinkIssuesInput(
+			RELEASE_TICKET, ticketID, "Relationship");
+
+			issueRestClient.linkIssue(linkIssuesInput);
+
+			if (ticketID.startsWith("LRCI") || ticketID.startsWith("LRQA")) {
+				boolean missingLabel = true;
+
+				for (String label : issue.getLabels()) {
+					if (label.startsWith("poshi_")) {
+						label = _upperCaseEachWord(
+							StringUtil.replace(label, "_", " "));
+
+						label = StringUtil.replace(label, "Poshi ", "");
+
+						if (label.equals("Pql")) {
+							label = StringUtil.upperCase(label);
+						}
+
+						missingLabel = false;
+
+						if (!ticketGroups.containsKey(label)) {
+							List<String> ticketList = new ArrayList<>();
+
+							ticketList.add(ticketMessage);
+
+							ticketGroups.put(label, ticketList);
+
+							break;
+						}
+
+						ticketGroups.get(
+							label
+						).add(
+							ticketMessage
+						);
+
+						break;
+					}
+				}
+
+				if (missingLabel) {
+					System.out.println(
+						"Missing poshi label: " + _getTicketURL(ticketID));
+
+					if (!ticketGroups.containsKey("Other")) {
 						List<String> ticketList = new ArrayList<>();
-
-						String ticketMessage = getTicketMarkdownURL(ticketID) +
-							" - " + issue.getSummary();
 
 						ticketList.add(ticketMessage);
 
-						ticketGroups.put(label, ticketList);
-
-						missingLabel = false;
+						ticketGroups.put("Other", ticketList);
 
 						break;
 					}
 
-					String ticketMessage = getTicketMarkdownURL(ticketID) +
-						" - " + issue.getSummary();
-
-					ticketGroups.get(label).add(ticketMessage);
-
-					missingLabel = false;
-
-					break;
+					ticketGroups.get(
+						"Other"
+					).add(
+						ticketMessage
+					);
 				}
 			}
+			else if (ticketID.startsWith("POSHI")) {
+				if ((issue.getComponents() == null) ||
+					!issue.getComponents().iterator().hasNext()) {
 
-			if (missingLabel) {
-				System.out.println(
-					"Missing poshi label: " + getTicketURL(ticketID));
+					System.out.println(
+						"Missing component: " + _getTicketURL(ticketID));
+				}
+
+				for (BasicComponent basicComponent : issue.getComponents()) {
+					String componentName = basicComponent.getName();
+
+					if (componentName.equals("Release")) {
+						break;
+					}
+
+					if (!ticketGroups.containsKey(componentName)) {
+						List<String> ticketList = new ArrayList<>();
+
+						ticketList.add(ticketMessage);
+
+						ticketGroups.put(componentName, ticketList);
+
+						break;
+					}
+
+					ticketGroups.get(
+						componentName
+					).add(
+						ticketMessage
+					);
+				}
+			}
+			else {
+				continue;
+
+//				if (!ticketGroups.containsKey("Other")) {
+//					List<String> ticketList = new ArrayList<>();
+//
+//					ticketList.add(ticketMessage);
+//
+//					ticketGroups.put("Other", ticketList);
+//
+//					continue;
+//				}
+//
+//				ticketGroups.get(
+//					"Other"
+//				).add(
+//					ticketMessage
+//				);
 			}
 		}
 
-		System.out.println("\nGenerated changelog:\n");
+		System.out.println(_getChangelogPost(ticketGroups, releaseVersion));
 
-		for (Map.Entry<String, List<String>> entry : ticketGroups.entrySet()) {
-			String label = entry.getKey();
+		String changeLogText = FileUtil.read(changelogFile);
 
-			label = upperCaseEachWord(label.replace("_", " "));
+		changeLogText = changeLogText.replaceFirst(
+			"# Poshi Runner Change Log\n",
+			_getChangelogText(ticketGroups, releaseVersion));
 
-			System.out.println("_" + label.replace("Poshi ", "") + "_");
-
-			for (String ticketMessage : entry.getValue()) {
-				System.out.println("* " + ticketMessage);
-			}
-
-			System.out.println("");
-		}
+		Files.write(changelogFile.toPath(), changeLogText.getBytes());
 
 		jiraRestClient.close();
 	}
 
-	public static String getTicketMarkdownURL(String ticketID) {
+	private static String _getChangelogPost(
+		Map<String, List<String>> ticketGroups, String releaseVersion) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("\n# Release: **[POSHI ");
+		sb.append(releaseVersion);
+		sb.append("](");
+		sb.append(_getTicketURL(RELEASE_TICKET));
+		sb.append(")**\n\n");
+
+		for (Map.Entry<String, List<String>> entry : ticketGroups.entrySet()) {
+			String label = entry.getKey();
+
+			sb.append("_" + label + "_\n");
+
+			for (String ticketMessage : entry.getValue()) {
+				sb.append("* " + ticketMessage + "\n");
+			}
+
+			sb.append("\n");
+		}
+
+		sb.append("## Additional Notes:\n");
+		sb.append("For more release notes click here:\n");
+		sb.append(
+			"https://github.com/liferay/liferay-portal/blob/master/modules/test/poshi-runner/CHANGELOG.markdown");
+
+		return sb.toString();
+	}
+
+	private static String _getChangelogText(
+		Map<String, List<String>> ticketGroups, String releaseVersion) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("# Poshi Runner Change Log\n");
+		sb.append("\n## " + releaseVersion + "\n");
+
+		for (Map.Entry<String, List<String>> entry : ticketGroups.entrySet()) {
+			String label = entry.getKey();
+
+			sb.append("\n### " + label + "\n");
+
+			for (String ticketMessage : entry.getValue()) {
+				sb.append("\n* " + ticketMessage);
+			}
+
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	private static String _getFileContentAtCommit(
+			Git git, RevCommit commit, String path)
+		throws IOException {
+
+		Repository repository = git.getRepository();
+
+		try (TreeWalk treeWalk = TreeWalk.forPath(
+				repository, path, commit.getTree())) {
+
+			ObjectId objectId = treeWalk.getObjectId(0);
+
+			ObjectLoader objectLoader = repository.open(objectId);
+
+			byte[] bytes = objectLoader.getBytes();
+
+			return new String(bytes, StandardCharsets.UTF_8);
+		}
+	}
+
+	private static String _getLastReleasedVersion(File changelogFile)
+		throws IOException {
+
+		if (_lastReleasedVersion != null) {
+			return _lastReleasedVersion;
+		}
+
+		BufferedReader b = new BufferedReader(new FileReader(changelogFile));
+
+		String readLine = "";
+
+		while ((readLine = b.readLine()) != null) {
+			if (readLine.startsWith("##")) {
+				String releaseVersion = RegexUtil.getGroup(
+					readLine, "##[\\s]*(.*)", 1);
+
+				releaseVersion = releaseVersion.trim();
+
+				String patchVersion = RegexUtil.getGroup(
+					releaseVersion, "[\\d]+\\.[\\d]+\\.([\\d]+)", 1);
+
+				Integer patchVersionInteger = Integer.parseInt(patchVersion);
+
+				patchVersionInteger++;
+
+				String newPatchVersion = patchVersionInteger.toString();
+
+				releaseVersion = releaseVersion.replace(
+					patchVersion, newPatchVersion);
+
+				_lastReleasedVersion = releaseVersion;
+
+				return _lastReleasedVersion;
+			}
+		}
+
+		throw new RuntimeException("Could not find last released version");
+	}
+
+	private static String _getTicketMarkdownURL(String ticketID) {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("[");
@@ -167,29 +431,31 @@ public class ChangelogGenerator {
 		sb.append("]");
 		sb.append("(");
 
-		sb.append(getTicketURL(ticketID));
+		sb.append(_getTicketURL(ticketID));
 
 		sb.append(")");
 
 		return sb.toString();
 	}
 
-	public static String getTicketURL(String ticketID) {
+	private static String _getTicketURL(String ticketID) {
 		return "https://issues.liferay.com/browse/" + ticketID;
 	}
 
-	public static String upperCaseEachWord(String s) {
+	private static String _upperCaseEachWord(String s) {
 		char[] chars = s.toCharArray();
 
 		chars[0] = Character.toUpperCase(chars[0]);
 
-		for(int x = 1; x < chars.length; x++) {
-			if(chars[x-1] == ' '){
+		for (int x = 1; x < chars.length; x++) {
+			if (chars[x - 1] == ' ') {
 				chars[x] = Character.toUpperCase(chars[x]);
 			}
 		}
 
 		return new String(chars);
 	}
+
+	private static String _lastReleasedVersion = null;
 
 }
